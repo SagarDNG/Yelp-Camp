9659c23900548c9230879079ca2610859a681d34
SagarDNG <sagardng123@gmail.com>
2024-07-14 20:18:15 +0530

    added db error message

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "ohh, No! connection error:"));
+db.on("error", console.error.bind(console, "ohh, No! Database connection error:"));
 db.once("open", () => {
     console.log("Hello, our DataBase is now connected :)");
 });
